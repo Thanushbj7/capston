0b6fd9f3bd61347b4fc2d6f011aae91493102e38
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-01-24 13:32:04 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,1 +1,25 @@
 
+@wire(getObject) wiredCases(value){
+    this.wiredRecords = value;
+    const { data, error } = value;
+
+    if(data){
+        let tempRecords = JSON.parse(JSON.stringify(data));
+
+        tempRecords = tempRecords.map(row => {
+            return {
+                ...row,
+                CaseNumber: row.Case__r.CaseNumber,
+                CreatedDate: row.Case__r.CreatedDate,
+                [row.Call_Activity__c]: row.Call_Type__c // Assigning Call_Type__c to its respective Call_Activity__c column
+            };
+        });
+
+        this.data = tempRecords;
+        console.log("tempRecords!", tempRecords);
+    }
+
+    if(error){
+        console.log("error Occurred!", error);
+    }
+}
