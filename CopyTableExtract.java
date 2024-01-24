365c969a277a7e373f518c1cf6776e2020c1ea05
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-01-24 12:54:39 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -40,3 +40,30 @@ Public with sharing class CaseRelatedListApex{
     }
     
 }
+
+
+
+
+
+
+
+
+<template>
+		<div style="height: 300px;">
+				<h2 slot="title">
+            <lightning-icon icon-name="action:new_note"  title=" Case History"></lightning-icon>
+           <b>Case History</b>
+        </h2>
+		
+		<lightning-datatable
+        data={data}
+        columns={columns}
+        key-field="Id"
+			  show-row-number-column
+			  row-number-offset={rowOffset}
+			  hide-checkbox-column       
+    >
+    </lightning-datatable>
+		</div>
+		 
+</template>
