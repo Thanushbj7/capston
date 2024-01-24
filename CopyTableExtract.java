c034a280a0de9c4a8a89217166ede010d3746e4a
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-01-24 12:57:41 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -67,3 +67,23 @@ Public with sharing class CaseRelatedListApex{
 		</div>
 		 
 </template>
+
+
+
+
+				  import { LightningElement,api,track,wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import EXAMPLE_MESSAGE_CHANNEL from '@salesforce/messageChannel/ExampleMessageChannel__c';
+import getObject from '@salesforce/apex/CaseRelatedListApex.getObject';
+//import getcaseactionrecords from '@salesforce/apex/CaseRelatedListApex.getcaseactionrecords';
+
+const columns = [
+    { label: 'Case Number', fieldName: 'CaseNumber' },
+    { label: 'Date', fieldName: 'CreatedDate' },
+    { label: 'Plan Id', fieldName: 'PlanID_Text__c' },
+    { label: 'Inquiry', fieldName: 'Call_Type__c' },
+    { label: 'Transactions', fieldName: 'Call_Type__c' },
+		 { label: 'Account Maintenance', fieldName: 'Call_Type__c' },
+		 { label: 'Forms', fieldName: 'Call_Type__c'},
+		{ label: 'Others', fieldName: 'Call_Type__c'},
+];
