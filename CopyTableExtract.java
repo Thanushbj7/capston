80f9ee1e18830499e358fa005c252d3271630256
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-01-24 13:38:33 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -23,3 +23,34 @@
         console.log("error Occurred!", error);
     }
 }
+
+
+
+
+
+@wire(getObject) wiredCases(value){
+    this.wiredRecords = value;
+    const { data, error } = value;
+
+    if(data){
+        let tempRecords = JSON.parse(JSON.stringify(data));
+        let activityMap = {};
+
+        tempRecords.forEach(row => {
+            const activity = row.Call_Activity__c;
+            if (!activityMap[activity]) {
+                activityMap[activity] = [];
+            }
+
+            activityMap[activity].push({ ...row, CaseNumber: row.Case__r.CaseNumber, CreatedDate: row.Case__r.CreatedDate });
+        });
+
+        // Flatten the map values into a single array
+        this.data = Object.values(activityMap).flat();
+        console.log("tempRecords!", this.data);
+    }
+
+    if(error){
+        console.log("error Occurred!", error);
+    }
+}
