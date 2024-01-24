8a3dc38ec9270b6f79054b93f5192c03e476f5e3
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-01-24 12:53:39 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,1 +1,42 @@
-
+Public with sharing class CaseRelatedListApex{
+   
+    @AuraEnabled(cacheable=true)
+    public static List<Case_Actions__c> getObject(){  
+        LIST< Case_Actions__c > objCaseList = new LIST< Case_Actions__c >(); 
+        
+        objCaseList =[SELECT  Case__r.CaseNumber,Case__r.CreatedDate,PlanID_Text__c,Call_Activity__c,Call_Type__c FROM Case_Actions__c where  Case__r.Account.SSN__c IN  ('010820241')];
+        
+        return  objCaseList;
+        
+          }
+    public static List<Case_Actions__c> getcaseactionrecords(){
+        
+        List<Case_Actions__c> Caseaction = new List<Case_Actions__c>();
+       // if((String.isBlank(Caseid) || Caseid==''||Caseid==null) &&(String.isBlank(clientStatus) || clientStatus==''||clientStatus==null)){
+            Caseaction = [SELECT  Case__r.CaseNumber,Case__r.CreatedDate,PlanID_Text__c,Call_Activity__c,Call_Type__c 
+                               from Case_Actions__c where  Case__r.Account.SSN__c IN  ('010820241') ];
+       
+       // }
+        
+        return generateWrapperData(Caseaction);
+    }
+    
+    private static List<Case_Actions__c> generateWrapperData(List<Case_Actions__c> Caseactionlist) {
+        List<Case_Actions__c> caseactionWrapperList = new List<Case_Actions__c>();
+        
+        for (Case_Actions__c Caction : Caseactionlist) {
+            
+            Case_Actions__c CactionWrap = new Case_Actions__c();
+            
+            CactionWrap.id = Caction.Id;
+            CactionWrap.PlanID_Text__c = Caction.PlanID_Text__c;
+            CactionWrap.Call_Activity__c = Caction.Call_Activity__c;
+            CactionWrap.Call_Type__c = Caction.Call_Type__c;          
+         
+            caseactionWrapperList.add(CactionWrap); 
+        }
+        
+        return caseactionWrapperList;
+    }
+    
+}
