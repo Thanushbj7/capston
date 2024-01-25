b3568b547c786c9c4de3af21e985ce228c25870b
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-01-25 17:09:55 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,3 +1,67 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import EXAMPLE_MESSAGE_CHANNEL from '@salesforce/messageChannel/ExampleMessageChannel__c';
+import getObject from '@salesforce/apex/CaseRelatedListApex.getObject';
+
+const columns = [
+    { label: 'Case Number', fieldName: 'CaseNumber' },
+    { label: 'Date', fieldName: 'CreatedDate' },
+    { label: 'Plan Id', fieldName: 'PlanID_Text__c' },
+    { label: 'Inquiry', fieldName: 'Call_Type__c' },
+    { label: 'Transactions', fieldName: 'Call_Type__c' },
+    { label: 'Account Maintenance', fieldName: 'Call_Type__c' },
+    { label: 'Forms', fieldName: 'Call_Type__c' },
+    { label: 'Others', fieldName: 'Call_Type__c' },
+];
+
+export default class CaseHistoryLWC extends LightningElement {
+    @track data = [];
+    @track columns = columns;
+    wiredRecords;
+
+    @wire(getObject) wiredCases(value) {
+        this.wiredRecords = value;
+        const { data, error } = value;
+        if (data) {
+            let tempRecords = JSON.parse(JSON.stringify(data));
+            let uniqueCallTypesMap = new Map();
+
+            tempRecords = tempRecords.map(row => {
+                const callType = row.Call_Type__c;
+                if (!uniqueCallTypesMap.has(callType)) {
+                    uniqueCallTypesMap.set(callType, true);
+
+                    return {
+                        ...row,
+                        CaseNumber: row.Case__r.CaseNumber,
+                        CreatedDate: row.Case__r.CreatedDate
+                    };
+                }
+
+                return null;
+            }).filter(row => row !== null);
+
+            this.data = tempRecords;
+            console.log("tempRecords!", tempRecords);
+        }
+
+        if (error) {
+            console.log("error Occurred!", error);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 import { LightningElement,api,track,wire } from 'lwc';
 import { publish, MessageContext } from 'lightning/messageService';
 import EXAMPLE_MESSAGE_CHANNEL from '@salesforce/messageChannel/ExampleMessageChannel__c';
@@ -64,3 +128,9 @@ wiredRecords;
 	
 
 }
+
+
+
+
+
+
