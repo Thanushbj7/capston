4a0c0263793d79ac10295c978d04ba6e72dab15b
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-01-25 17:23:18 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -7,6 +7,59 @@ const columns = [
     { label: 'Case Number', fieldName: 'CaseNumber' },
     { label: 'Date', fieldName: 'CreatedDate' },
     { label: 'Plan Id', fieldName: 'PlanID_Text__c' },
+    { label: 'Inquiry', fieldName: 'Inquiry' },
+    { label: 'Transactions', fieldName: 'Transactions' },
+    { label: 'Account Maintenance', fieldName: 'AccountMaintenance' },
+    { label: 'Forms', fieldName: 'Forms' },
+    { label: 'Others', fieldName: 'Others' },
+];
+
+export default class CaseHistoryLWC extends LightningElement {
+    @track data = [];
+    @track columns = columns;
+    wiredRecords;
+
+    @wire(getObject) wiredCases(value) {
+        this.wiredRecords = value;
+        const { data, error } = value;
+        if (data) {
+            let tempRecords = JSON.parse(JSON.stringify(data));
+
+            tempRecords = tempRecords.map(row => {
+                const callType = row.Call_Type__c;
+                return {
+                    ...row,
+                    CaseNumber: row.Case__r.CaseNumber,
+                    CreatedDate: row.Case__r.CreatedDate,
+                    Inquiry: callType === 'Inquiry' ? callType : '',
+                    Transactions: callType === 'Transactions' ? callType : '',
+                    AccountMaintenance: callType === 'Account Maintenance' ? callType : '',
+                    Forms: callType === 'Forms' ? callType : '',
+                    Others: callType === 'Others' ? callType : ''
+                };
+            });
+
+            this.data = tempRecords;
+            console.log("tempRecords!", tempRecords);
+        }
+
+        if (error) {
+            console.log("error Occurred!", error);
+        }
+    }
+}
+
+
+
+import { LightningElement, api, track, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import EXAMPLE_MESSAGE_CHANNEL from '@salesforce/messageChannel/ExampleMessageChannel__c';
+import getObject from '@salesforce/apex/CaseRelatedListApex.getObject';
+
+const columns = [
+    { label: 'Case Number', fieldName: 'CaseNumber' },
+    { label: 'Date', fieldName: 'CreatedDate' },
+    { label: 'Plan Id', fieldName: 'PlanID_Text__c' },
     { label: 'Inquiry', fieldName: 'Call_Type__c' },
     { label: 'Transactions', fieldName: 'Call_Type__c' },
     { label: 'Account Maintenance', fieldName: 'Call_Type__c' },
