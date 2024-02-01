5e861b96ff5faa25a92dc25d95b2c5410c38be69
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-02-01 11:58:25 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,1 +1,68 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import EXAMPLE_MESSAGE_CHANNEL from '@salesforce/messageChannel/ExampleMessageChannel__c';
+import getObject from '@salesforce/apex/CaseRelatedListApex.getObject';
 
+
+const columns = [
+    { label: 'Case Number', fieldName: 'CaseNumber' },
+    { label: 'Date', fieldName: 'CreatedDate' },
+    { label: 'Plan Id', fieldName: 'PlanID_Text__c' },
+    { label: 'Inquiry', fieldName: 'Call_Type__c' },
+    { label: 'Transactions', fieldName: 'Call_Type__c' },
+    { label: 'Account Maintenance', fieldName: 'Call_Type__c' },
+    { label: 'Forms', fieldName: 'Call_Type__c' },
+    { label: 'Others', fieldName: 'Call_Type__c' },
+];
+
+export default class CaseHistoryLWC extends LightningElement {
+
+
+
+    @track data = [];
+    @track columns = columns;
+    wiredRecords;
+    @wire(getObject) wiredCases(value) {
+        this.wiredRecords = value;
+        const { data, error } = value;
+        if (data) {
+
+            let tempRecords = JSON.parse(JSON.stringify(data));
+
+            let uniqueCaseNumbers = new Set();
+            let uniquePlanIds = new Set();
+            let uniqueCallTypesMap = new Map();
+
+            tempRecords = tempRecords.map(row => {
+
+
+                if (!uniquePlanIds.has(row.PlanID_Text__c)) {
+                    uniquePlanIds.add(row.PlanID_Text__c);
+
+
+                    return {...row, CaseNumber: row.Case__r.CaseNumber, CreatedDate: row.Case__r.CreatedDate };
+                    //console.log(tempRecords);
+                }
+
+                return {...row, PlanID_Text__c: null };
+            });
+
+
+
+            this.data = tempRecords;
+            console.log("tempRecords!", tempRecords);
+
+
+        }
+
+        if (error) {
+            console.log("error Occurred!", error);
+        }
+    }
+
+
+
+
+
+
+}
