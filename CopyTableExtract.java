e2d770f14df191dd464ff7062d3ff44847fcb663
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-02-13 17:28:33 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,4 +1,22 @@
-Map<String ,GenericWithdrawal__c> GWcustomMap = new Map<String ,GenericWithdrawal__c>();
+global static ResponseWrapper populatePAAG() {
+        System.debug('POPULATE PAAG');
+        
+        //For testing puposed the XML Data is read from Static resource instead pulling from request post
+        //StaticResource sr = [Select Body From StaticResource Where Name = 'SETITDATA' LIMIT 1];
+        RestRequest req = RestContext.request;
+        
+        //String xmlData = sr.body.toString();
+        String xmlData = req.requestBody.toString();
+        if(!String.isBlank(xmlData)){
+            initSchemaFieldMap();
+            
+            Map<String ,PAAG__c> paagMap = new Map<String ,PAAG__c>();
+            for(PAAGSetting__c  paagSetting : PAAGSetting__c.getall().values()){
+                paagSettingMap.put(paagSetting.API_Name__c,paagSetting);
+                
+            }
+           
+           Map<String ,GenericWithdrawal__c> GWcustomMap = new Map<String ,GenericWithdrawal__c>();
             for(GenericWithdrawals__c  GWCustomSetting : GenericWithdrawals__c.getall().values()){
               if(GWCustomSetting.API_Name__c!= null){
                 GWSettingMap.put(GWCustomSetting.API_Name__c,GWCustomSetting);
@@ -6,6 +24,25 @@ Map<String ,GenericWithdrawal__c> GWcustomMap = new Map<String ,GenericWithdrawa
             }
        
             }
+            
+           //  system.debug('PAAGpaagSettingMap>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>' +paagSettingMap.get('a2ve00000015YwNAAU'));
+            system.debug('PAAG Size' +paagMap.size() );
+            parseXMLData(xmlData, paagMap);
+            system.debug('PAAG Size' +paagMap.size() );
+           
+            addPlanPAAGInfo(paagMap);
+        }
+        
+        ResponseWrapper resWrapper = new ResponseWrapper();
+        if(paagResultMap.size() > 0){
+            resWrapper.resultInfos = paagResultMap.values();
+        }
+        return resWrapper;
+        
+    }
+
+if(GWCustomSetting.API_Name__c!= null){
+                GWSettingMap.put(GWCustomSetting.API_Name__c,GWCustomSetting);
 
 
 
