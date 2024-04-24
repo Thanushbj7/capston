593d9cf3cd01fd65808aeea4e97b7b7a7dac8273
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-04-24 15:58:17 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,3 +1,62 @@
+public ArcticleEditSuggestPlanController() {
+        //this.articleCase = new Case();
+         Case articleCase=new Case();
+         system.debug('pageType===================================================================================='+ articleCase);
+     	pageType =  ApexPages.currentPage().getParameters().get('pageType');
+     	Profile p = [Select Name from Profile where Id =: userinfo.getProfileid()];
+		profileName = p.name;
+     	system.debug('pageType'+ pageType);
+     	if(pageType != null && pageType == 'PAAG'){
+     		RecordType caseRecType = [Select Id, Name, SobjectType from RecordType where (SobjectType ='Case') and (Name ='PAAG Modification Request') limit 1];
+       		articleCase.RecordTypeId = caseRecType.Id;
+     	 	planID = ApexPages.currentPage().getParameters().get('planId');
+     	 	system.debug('planId'+ planId);
+     	 	String paagId = ApexPages.currentPage().getParameters().get('paagID');
+     	 	system.debug('paagID'+ paagId);
+     	 	planNumber = ApexPages.currentPage().getParameters().get('planNumber');
+     	 	system.debug('planNumber'+ planNumber);
+     	 	planName = ApexPages.currentPage().getParameters().get('planName');
+     	 	market =  ApexPages.currentPage().getParameters().get('market');
+     	 	
+     	 	system.debug('planName'+ planName);
+     	 	articleCase.Plan_ID__c = planID;
+     	 	articleCase.subject = 'PAAG Edit Request';
+     	 	articleCase.Article_Url__c = 'https://' + ApexPages.currentPage().getHeaders().get('Host')+ '/' + paagId;
+     	 	articleCase.Market__c = market;
+     	 	queueName = 'PAAG & Article Edit Request Queue';
+     	}
+     	else {
+     	 	RecordType caseRecType = [Select Id, Name, SobjectType from RecordType where (SobjectType ='Case') and (Name ='CCC KM Article Update Request') limit 1];
+       		articleCase.RecordTypeId = caseRecType.Id;
+	    	//articleCase.Article_Url__c = ApexPages.currentPage().getParameters().get('articleUrl');
+	     	articleCase.Article_Name__c = ApexPages.currentPage().getParameters().get('articleName');
+	     	System.debug('Article Name' + articleCase.Article_Name__c);
+	     	String articleType =  ApexPages.currentPage().getParameters().get('articleType');
+	     	System.debug('Article Type' + articleType);
+	     	if('Job_Aid__kav'.equals(articleType)){
+	     		queueName = 'Job Aid Article Edit Queue';
+	     	 	articleCase.subject = 'Job Aid Edit Request';
+	     	}
+	     	else {
+	     	 	queueName = 'PAAG & Article Edit Request Queue';
+	     	 	articleCase.subject =  'Plan Article Edit Request';
+	     	}
+	     	articleType =  articleType.Substring(0, articleType.length()-5);
+	     	articleCase.Article_Type__c = articleType;     	
+	     	articleCase.Article_Url__c = 'https://' + ApexPages.currentPage().getHeaders().get('Host')+ '/articles/' + articleType + '/' + articleCase.Article_Name__c;
+     	}
+        articleCase.Status = 'New';
+     	Apexpages.currentPage().getHeaders().put('X-UA-Compatible', 'IE=edge');
+    }
+
+
+
+
+
+
+
+
+
 <template>
     <!-- Add any necessary imports here -->
     <lightning-card title="{if: true}" variant="Narrow">
