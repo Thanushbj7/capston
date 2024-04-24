823b8fb158faaa467fda4b06c33c842461b544a2
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-04-24 16:00:30 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,3 +1,58 @@
+public void initializeArticleEditSuggestPlan() {
+    Case articleCase = new Case();
+    
+    pageType = ApexPages.currentPage().getParameters().get('pageType');
+    Profile p = [SELECT Name FROM Profile WHERE Id = :UserInfo.getProfileId()];
+    profileName = p.Name;
+    System.debug('pageType: ' + pageType);
+    
+    if (pageType != null && pageType == 'PAAG') {
+        RecordType caseRecType = [SELECT Id, Name, SobjectType FROM RecordType WHERE SObjectType = 'Case' AND Name = 'PAAG Modification Request' LIMIT 1];
+        articleCase.RecordTypeId = caseRecType.Id;
+        planID = ApexPages.currentPage().getParameters().get('planId');
+        System.debug('planId: ' + planId);
+        String paagId = ApexPages.currentPage().getParameters().get('paagID');
+        System.debug('paagID: ' + paagId);
+        planNumber = ApexPages.currentPage().getParameters().get('planNumber');
+        System.debug('planNumber: ' + planNumber);
+        planName = ApexPages.currentPage().getParameters().get('planName');
+        market = ApexPages.currentPage().getParameters().get('market');
+        
+        System.debug('planName: ' + planName);
+        articleCase.Plan_ID__c = planID;
+        articleCase.Subject = 'PAAG Edit Request';
+        articleCase.Article_Url__c = 'https://' + ApexPages.currentPage().getHeaders().get('Host') + '/' + paagId;
+        articleCase.Market__c = market;
+        queueName = 'PAAG & Article Edit Request Queue';
+    } else {
+        RecordType caseRecType = [SELECT Id, Name, SobjectType FROM RecordType WHERE SObjectType = 'Case' AND Name = 'CCC KM Article Update Request' LIMIT 1];
+        articleCase.RecordTypeId = caseRecType.Id;
+        // articleCase.Article_Url__c = ApexPages.currentPage().getParameters().get('articleUrl');
+        articleCase.Article_Name__c = ApexPages.currentPage().getParameters().get('articleName');
+        System.debug('Article Name: ' + articleCase.Article_Name__c);
+        String articleType = ApexPages.currentPage().getParameters().get('articleType');
+        System.debug('Article Type: ' + articleType);
+        if ('Job_Aid__kav'.equals(articleType)) {
+            queueName = 'Job Aid Article Edit Queue';
+            articleCase.Subject = 'Job Aid Edit Request';
+        } else {
+            queueName = 'PAAG & Article Edit Request Queue';
+            articleCase.Subject = 'Plan Article Edit Request';
+        }
+        articleType = articleType.substring(0, articleType.length() - 5);
+        articleCase.Article_Type__c = articleType;
+        articleCase.Article_Url__c = 'https://' + ApexPages.currentPage().getHeaders().get('Host') + '/articles/' + articleType + '/' + articleCase.Article_Name__c;
+    }
+    articleCase.Status = 'New';
+    ApexPages.currentPage().getHeaders().put('X-UA-Compatible', 'IE=edge');
+}
+
+
+
+
+
+
+
 public ArcticleEditSuggestPlanController() {
         //this.articleCase = new Case();
          Case articleCase=new Case();
